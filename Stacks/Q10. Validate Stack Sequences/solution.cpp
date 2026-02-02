d44448910f5af1c0d28ffb2782a59a2eb2112baf
Lakshita Aggarwal <lakshitaaggarwal15@gmail.com>
2026-02-02 21:25:50 +0530

    Create solution.cpp

---
+++ Stacks/Q10. Validate Stack Sequences/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>s;
+        int i=0,j=0;
+        while(i<pushed.size()){
+            s.push(pushed[i]);
+            i++;
+            while(!s.empty() && s.top()==popped[j]){
+                s.pop();
+                j++;
+            }
+
+        }
+
+        return s.size()==0;
+        
+    }
+};
